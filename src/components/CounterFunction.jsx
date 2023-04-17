cba1316a200229146754a5143f30eddbdab7ef64
Nana Kwaku <nanadaasebrekwaku@gamil.com>
2023-04-17 15:31:44 +0000

    new commit

---
+++ src/components/CounterFunction.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import './Counter.css';
+
+function CounterFunction() {
+    const [count, setCount] = useState(0);
+ 
+ const Decrease= () =>{
+    setCount(count - 1)
+ }
+
+    return (
+    <div className='CounterApp'>
+        <h1>Counter App</h1>
+        <h1>Count : {count}</h1>
+        <div className='buttons'>
+         <button className='button' onClick={() =>setCount(count + 1)}>+</button>
+         <button className='button' onClick={Decrease}>-</button>
+        </div>
+        
+    </div>
+  )
+}
+
+export default CounterFunction
